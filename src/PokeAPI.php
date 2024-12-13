70aecb967257c0dd501186aee65340109dbb9fa6
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-12-13 00:57:18 +0000

    connector

---
+++ src/PokeAPI.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PokeApiSdk;
+
+use Saloon\Http\Connector;
+
+class PokeAPI extends Connector
+{
+    public function resolveBaseUrl(): string
+    {
+        return 'https://pokeapi.co/api/v2/';
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+    }
+}
