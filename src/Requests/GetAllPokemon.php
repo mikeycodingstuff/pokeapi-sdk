e5e693f67a873a8ec7262efc99502652a6a5c5a0
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-12-13 00:57:23 +0000

    get all pokemon request

---
+++ src/Requests/GetAllPokemon.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PokeApiSdk\Requests;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetAllPokemon extends Request
+{
+    /**
+     * HTTP Method
+     */
+    protected Method $method = Method::GET;
+
+    /**
+     * Resolve the endpoint
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/pokemon';
+    }
+}
