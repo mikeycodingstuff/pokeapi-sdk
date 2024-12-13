b5e6e0328ebd912d8796dc0c51169ed0069db19f
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2024-12-13 01:09:18 +0000

    pokemon resource used in connector

--- src/PokeAPI.php
+++ src/PokeAPI.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PokeApiSdk;
 
+use PokeApiSdk\Resources\PokemonResource;
 use Saloon\Http\Connector;
 
 class PokeAPI extends Connector
@@ -19,5 +20,10 @@ class PokeAPI extends Connector
             'Accept' => 'application/json',
             'Content-Type' => 'application/json',
         ];
+    }
+
+    public function pokemon(): PokemonResource
+    {
+        return new PokemonResource($this);
     }
 }
