4ae32b45cd6c9d8ff7916cebe3088726bffd4b53
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2026-09-12 10:29:14 +0100

    chore: update rector rules

--- rector.php
+++ rector.php
@@ -5,13 +5,32 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
+    ->withCache(
+        // specify a path that works locally as well as on CI job runners
+        cacheDirectory: '/tmp/rector'
+    )
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
-    // uncomment to reach your current PHP version
     ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0)
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        typeDeclarationDocblocks: true,
+        privatization: true,
+        naming: true,
+        namedArgs: true,
+        carbon: true,
+        rectorPreset: true,
+        phpunitCodeQuality: true,
+        phpunitNarrowAsserts: true,
+        phpunitMockToStub: true,
+        doctrineCodeQuality: true,
+        symfonyCodeQuality: true,
+        symfonyConfigs: true,
+    )
+    ->withAttributesSets()
     ->withImportNames();
